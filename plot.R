0b2e09da3a9d4c49b80af38357a90fe83407d9d1
John Wade <john@home>
2015-03-18 20:44:01 -0400

    First draft plot 3.

--- plot.R
+++ plot.R
@@ -29,7 +29,30 @@ plot.2 <- function() {
              main="PM2.5 Total Emissions in Baltimore City, Maryland",
              xlab="Year", ylab="PM2.5 (tons)"
              )
-        #axis(2, at=axTicks(2), labels=axTicks(2)/1e3)
         abline(lm(total ~ year, data))
+        data
+}
+
+# Of the four types of sources indicated by the type (point, nonpoint, onroad,
+# nonroad) variable, which of these four sources have seen decreases in
+# emissions from 1999–2008 for Baltimore City? Which have seen increases in
+# emissions from 1999–2008? Use the ggplot2 plotting system to make a plot
+# answer this question.
+plot.3 <- function() {
+        data <- nei %>%
+                filter(fips == "24510") %>%
+                group_by(type, year) %>%
+                summarize(total=sum(Emissions))
+
+        g <- ggplot(aes(year, total), data = data) +
+                ggtitle("PM2.5 Emission in Baltimore City, Maryland") +
+                xlab("Year") +
+                ylab("PM2.5 (tons)") +
+                geom_point() +
+                facet_grid(~ type) +
+                stat_smooth(method="lm")
+
+
+        print(g)
         data
 }
